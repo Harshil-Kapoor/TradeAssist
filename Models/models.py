f904ef396b9ac490f7f9e84a1ef97a9d536adc22
Harshil-Kapoor <harshilkapoor@gmail.com>
2021-09-13 02:04:25 +0530

    models: Add Position and Holding objects.

--- Models/models.py
+++ Models/models.py
@@ -1,3 +1,5 @@
+from Utils.SmartAPI import get_connection, get_current_value
+
 class OrderParams:
     def __init__(self, obj):
         """ Class defining Order structure\n
@@ -89,7 +91,7 @@ class HistoryParams:
                 "exchange": "NSE",
                 "symboltoken": "3045",
                 "interval": "ONE_MINUTE",
-                "fromdate": "2021-02-08 09:00", 
+                "fromdate": "2021-02-08 09:00",
                 "todate": "2021-02-08 09:16"\n
             }
         """
@@ -114,7 +116,7 @@ class Candle:
         """ Class defining paramteres for a candle\n
             Accepts Structure:\n
             [
-                "2021-02-10T09:15:00+05:30",
+               "2021-02-10T09:15:00+05:30",
                394.05,
                397.7,
                394,
@@ -152,3 +154,160 @@ class Candle:
 
     def get_body_ratio(self):
         return self.body / self.size
+    
+    def get_summary(self):
+        return f'''Timestamp: {self.timestamp}\n
+            Open: {self.open}\n
+            High: {self.high}\n
+            Low: {self.low}\n
+            Close: {self.close}\n
+            Volume: {self.volume}'''
+
+class Postion:
+    def __init__(self, obj):
+        """ Class defining paramteres for a Position\n
+            Accepts Structure:\n
+            {
+               "exchange": "NSE",
+               "symboltoken": "2885",
+               "producttype": "DELIVERY",
+               "tradingsymbol": "RELIANCE-EQ",
+               "symbolname": "RELIANCE",
+               "instrumenttype": "",
+               "priceden": "1",
+               "pricenum": "1",
+               "genden": "1",
+               "gennum": "1",
+               "precision": "2",
+               "multiplier": "-1",
+               "boardlotsize": "1",
+               "buyqty": "1",
+               "sellqty": "0",
+               "buyamount": "2235.80",
+               "sellamount": "0",
+               "symbolgroup": "EQ",
+               "strikeprice": "-1",
+               "optiontype": "",
+               "expirydate": "",
+               "lotsize": "1",
+               "cfbuyqty": "0",
+               "cfsellqty": "0",
+               "cfbuyamount": "0",
+               "cfsellamount": "0",
+               "buyavgprice": "2235.80",
+               "sellavgprice": "0",
+               "avgnetprice": "2235.80",
+               "netvalue": "- 2235.80",
+               "netqty": "1",
+               "totalbuyvalue": "2235.80",
+               "totalsellvalue": "0",
+               "cfbuyavgprice": "0",
+               "cfsellavgprice": "0",
+               "totalbuyavgprice": "2235.80",
+               "totalsellavgprice": "0",
+               "netprice": "2235.80"
+            }
+        """
+        self.keys = [
+            "exchange",
+            "symboltoken",
+            "tradingsymbol",
+            "instrumenttype",
+            "buyqty",
+            "sellqty",
+            "strikeprice",
+            "lotsize",
+            "buyavgprice",
+            "sellavgprice",
+            "avgnetprice",
+            "netvalue",
+            "netqty",
+            "totalbuyvalue",
+            "totalsellvalue",
+            "totalbuyavgprice",
+            "totalsellavgprice",
+            "netprice"
+        ]
+
+        dict = {k: v for k,v in zip(self.keys, obj)}
+        for k, v in dict.items():
+            if k in self.keys:
+                self.__setattr__(k, v)
+        
+        connection, data = get_connection()
+        self.current = get_current_value(connection, self)
+        self.pl = self.current - self.netvalue
+        self.netchange = self.pl / self.avgnetprice
+        self.position = "BUY" if self.totalbuyvalue != 0 else "SELL"
+
+        self.keys.insert("current")
+        self.keys.insert("pl")
+        self.keys.insert("netchange")
+        self.keys.insert("position")
+
+    def get_dict(self):
+        return {k: self.__dict__[k] for k in self.keys}
+
+    def get_summary(self):
+        if self.position is "BUY":
+            return f'''Symbol: {self.tradingsymbol}
+                Avg. Buy Price: {self.buyavgprice}\n
+                Current Price: {self.current}\n
+                Quantity: {self.buyqty}\n
+                Net Price: {self.netprice}\n
+                P&L: {self.pl}\n
+                Net Change: {self.netchange}'''
+        else:
+            return f'''Symbol: {self.tradingsymbol}
+                Avg. Sell Price: {self.sellavgprice}\n
+                Current Price: {self.current}\n
+                Quantity: {self.sellqty}\n
+                Net Price: {self.netprice}\n
+                Net Change: {self.netchange}'''
+
+class Holding:
+    def __init__(self, obj):
+        """ Class defining paramteres for a Holding\n
+            Accepts Structure:\n
+            {
+                "tradingSymbol":"RELIANCE-EQ",
+                "exchange":"NSE",
+                "isin":"0INE002A01018",
+                "t1quantity":"0",
+                "realisedquantity":"0",
+                "quantity":"0",
+                "authorisedquantity":"0",
+                "profitandloss":"0",
+                "product":"DELIVERY",
+                "collateralquantity":"0",
+                "collateraltype":"null",
+                "haircut":"0",
+            }
+        """
+        self.keys = [
+            "tradingSymbol",
+            "exchange",
+            "isin",
+            "t1quantity",
+            "realisedquantity",
+            "quantity",
+            "authorisedquantity",
+            "profitandloss",
+            "product",
+            "collateralquantity",
+            "collateraltype",
+            "haircut"
+        ]
+
+        dict = {k: v for k,v in zip(self.keys, obj)}
+        for k, v in dict.items():
+            if k in self.keys:
+                self.__setattr__(k, v)
+
+    def get_dict(self):
+        return {k: self.__dict__[k] for k in self.keys}
+
+    def get_summary(self):
+        return f'''Symbol: {self.tradingsymbol}
+            Quantity: {self.quantity}\n
+            P&L: {self.profitandloss}'''
