72a200a72dd96a2451d33976c669aeb9f6c36249
Harshil-Kapoor <harshilkapoor@gmail.com>
2021-10-13 21:49:49 +0530

    Utils: Properly decode positions and holdings.

--- Utils/Utils.py
+++ Utils/Utils.py
@@ -1,3 +1,4 @@
+import json
 from typing import List
 from Models.models import Candle, Holding, Postion
 
@@ -7,8 +8,8 @@ def get_candles(candleData: List[List]) -> List[Candle]:
 
 
 def format_positions(positions: bytes) -> List[Postion]:
-    return [Postion(position) for position in positions["data"]]
+    return [Postion(position) for position in json.loads(positions.decode('utf-8'))["data"]]
 
 
 def format_holdings(holdings: bytes) -> List[Holding]:
-    return [Holding(holding) for holding in holdings["data"]]
+    return [Holding(holding) for holding in json.loads(holdings.decode('utf-8'))["data"]]
